6cd4c0c022cc232b7820f67239b3412cf2cd4b0a
Laon <laon.fedev@gmail.com>
2023-02-24 19:05:55 +0900

    Feat: GRAPHQL-API none hardcoding

--- pages/04-2/index.js
+++ pages/04-2/index.js
@@ -1,8 +1,9 @@
 import { useMutation, gql } from '@apollo/client';
+import { useState } from 'react';
 
 const CREATE_BOARD = gql`
-  mutation {
-    createBoard(writer: "ㅇㅂㅇ", title: "으갸갸갸", contents: "호잇") {
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
       _id
       number
       message
@@ -11,17 +12,44 @@ const CREATE_BOARD = gql`
 `;
 
 export default function GraphqlPracticePage() {
+  const [writer, setWriter] = useState('');
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
+
   const [createBoard] = useMutation(CREATE_BOARD);
 
   const onClickSubmit = async () => {
-    const result = await createBoard();
+    const result = await createBoard({
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
     console.log(result);
-    console.log(result.data.createBoard.message);
-    console.log(result.data.createBoard.number);
+    alert(result.data.createBoard.message);
+  };
+
+  const onChangeWriter = (e) => {
+    setWriter(e.target.value);
+  };
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeContents = (e) => {
+    setContents(e.target.value);
   };
 
   return (
     <>
+      작성자: <input type="text" onChange={onChangeWriter} />
+      <br />
+      제목: <input type="text" onChange={onChangeTitle} />
+      <br />
+      내용: <input type="text" onChange={onChangeContents} />
+      <br />
       <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
     </>
   );
