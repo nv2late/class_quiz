ef1718b4186a03af4624722f1724a9737144f0b7
Laon <laon.fedev@gmail.com>
2023-02-24 18:53:29 +0900

    Feat: GRAPHQL-API 하드코딩으로 데이터 받아오기

--- pages/04-2/index.js
+++ pages/04-2/index.js
@@ -1,9 +1,28 @@
 import { useMutation, gql } from '@apollo/client';
 
+const CREATE_BOARD = gql`
+  mutation {
+    createBoard(writer: "ㅇㅂㅇ", title: "으갸갸갸", contents: "호잇") {
+      _id
+      number
+      message
+    }
+  }
+`;
+
 export default function GraphqlPracticePage() {
+  const [createBoard] = useMutation(CREATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await createBoard();
+    console.log(result);
+    console.log(result.data.createBoard.message);
+    console.log(result.data.createBoard.number);
+  };
+
   return (
     <>
-      <button onClick={test}>GRAPHQL-API 요청하기</button>
+      <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
     </>
   );
 }
