fccbd0b080a6a926ec817595efbace08356e3917
Laon <laon.fedev@gmail.com>
2023-03-03 12:52:09 +0900

    section6 Quiz: 1 상품 등록 화면 구성

---
+++ pages/05/boards/index.js
@@ -0,0 +1,16 @@
+export default function DynamicRoutingPracticePage() {
+  return (
+    <>
+      <h1>상품 등록 페이지</h1>
+      판매자: <input type="text" />
+      <br />
+      상품명: <input type="text" />
+      <br />
+      상품내용: <input type="text" />
+      <br />
+      상품가격: <input type="text" />
+      <br />
+      <button>상품을 등록합니다.</button>
+    </>
+  );
+}
