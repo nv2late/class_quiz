ed70804e463a991ae517346e17e678fab42fda6a
Laon <laon.fedev@gmail.com>
2023-03-12 17:19:16 +0900

    Add: Day7 Quiz Setup

---
+++ pages/07/boards/index.js
@@ -0,0 +1,5 @@
+import DynamicRoutingPracticeContainer from '@/src/components/units/board/routing/DynamicRouting.container';
+
+export default function DynamicRoutingPracticePage() {
+  return <DynamicRoutingPracticeContainer />;
+}
